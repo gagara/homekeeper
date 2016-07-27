7464f03fdc8d4321cfabd69b22f6a46c6a5f4c71
Viacheslav Gagara <viacheslavg@gmail.com>
2016-07-27 18:50:17 +0300

    ongoing updates

--- client/android/HomeManager/src/com/gagara/homekeeper/nbi/service/ProxyNbiService.java
+++ client/android/HomeManager/src/com/gagara/homekeeper/nbi/service/ProxyNbiService.java
@@ -1,33 +1,42 @@
 package com.gagara.homekeeper.nbi.service;
 
+import static com.android.volley.Request.Method.POST;
 import static com.gagara.homekeeper.common.Constants.MESSAGE_KEY;
 import static com.gagara.homekeeper.common.Constants.TIMESTAMP_KEY;
 import static com.gagara.homekeeper.nbi.service.ServiceState.ACTIVE;
 import static com.gagara.homekeeper.nbi.service.ServiceState.ERROR;
 import static com.gagara.homekeeper.nbi.service.ServiceState.INIT;
 
+import java.io.UnsupportedEncodingException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.IntentFilter;
 import android.support.v4.content.LocalBroadcastManager;
+import android.util.Base64;
 import android.util.Log;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.Response.ErrorListener;
 import com.android.volley.Response.Listener;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.Volley;
@@ -45,8 +54,7 @@ public class ProxyNbiService extends AbstractNbiService {
     private static final int HTTP_CONNECTION_TIMEOUT = 5000;
 
     private static final String PROXY_DATE_FORMAT = "yyyy-MM-ddTHH:mm:ss.SSS'Z'";
-    private static DateFormat df = new SimpleDateFormat(PROXY_DATE_FORMAT,
-            Locale.getDefault());
+    private static DateFormat df = new SimpleDateFormat(PROXY_DATE_FORMAT, Locale.getDefault());
 
     private Proxy proxy = null;
     private RequestQueue httpRequestQueue = null;
@@ -92,35 +100,14 @@ public class ProxyNbiService extends AbstractNbiService {
 
     @Override
     public void send(Request request) {
-        JsonObjectRequest req = new JsonObjectRequest(
-                com.android.volley.Request.Method.POST, proxy.asUrl(),
-                request.toJson(), new Response.Listener<JSONObject>() {
-
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        // do nothing
-                    }
-                }, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError e) {
-                        Log.e(TAG, e.getMessage(), e);
-                        state = ERROR;
-                        synchronized (serviceExecutor) {
-                            serviceExecutor.notifyAll();
-                        }
-                    }
-                });
-        req.setRetryPolicy(new DefaultRetryPolicy(HTTP_CONNECTION_TIMEOUT,
-                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+        JsonObjectRequest req = new ProxySendRequest(request);
+        req.setRetryPolicy(new DefaultRetryPolicy(HTTP_CONNECTION_TIMEOUT, DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
                 DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-        // add basic auth
         httpRequestQueue.add(req);
     }
 
     private void runService() throws InterruptedException {
-        LocalBroadcastManager.getInstance(this).registerReceiver(
-                controllerCommandReceiver,
+        LocalBroadcastManager.getInstance(this).registerReceiver(controllerCommandReceiver,
                 new IntentFilter(Constants.CONTROLLER_CONTROL_COMMAND_ACTION));
 
         lastMessageTimestamp = new Date();
@@ -130,10 +117,9 @@ public class ProxyNbiService extends AbstractNbiService {
         while (true) {
             if (state == INIT || state == ERROR) {
                 clocksDelta = null;
-                clockSyncExecutor = Executors
-                        .newSingleThreadScheduledExecutor();
-                clockSyncExecutor.scheduleAtFixedRate(new SyncClockRequest(),
-                        0L, proxy.getPullPeriod() * 3, TimeUnit.SECONDS);
+                clockSyncExecutor = Executors.newSingleThreadScheduledExecutor();
+                clockSyncExecutor.scheduleAtFixedRate(new SyncClockRequest(), 0L, proxy.getPullPeriod() * 3,
+                        TimeUnit.SECONDS);
                 notifyStatusChange(getText(R.string.service_sync_clocks_status));
                 state = ACTIVE;
             }
@@ -151,50 +137,9 @@ public class ProxyNbiService extends AbstractNbiService {
                 @Override
                 public void run() {
                     try {
-                        JsonRequest<JSONArray> req = new JsonLogRequest(
-                                com.android.volley.Request.Method.POST, proxy
-                                        .asUrl(), new LogRequest(
-                                        lastMessageTimestamp).toJson()
-                                        .toString(),
-                                new Response.Listener<JSONArray>() {
-
-                                    @Override
-                                    public void onResponse(JSONArray response) {
-                                        try {
-                                            for (int i = 0; i < response
-                                                    .length(); i++) {
-                                                JSONObject log = response
-                                                        .getJSONObject(i);
-                                                JSONObject message = log
-                                                        .getJSONObject(MESSAGE_KEY);
-                                                Date timestamp = df.parse(log
-                                                        .getString(TIMESTAMP_KEY));
-                                                processMessage(message,
-                                                        timestamp);
-                                            }
-                                        } catch (Exception e) {
-                                            Log.e(TAG,
-                                                    "failed to process ["
-                                                            + response + "]: "
-                                                            + e.getMessage(), e);
-                                        }
-                                    }
-                                }, new Response.ErrorListener() {
-
-                                    @Override
-                                    public void onErrorResponse(VolleyError e) {
-                                        Log.e(TAG, e.getMessage(), e);
-                                        state = ERROR;
-                                        synchronized (serviceExecutor) {
-                                            serviceExecutor.notifyAll();
-                                        }
-                                    }
-                                });
-                        req.setRetryPolicy(new DefaultRetryPolicy(
-                                HTTP_CONNECTION_TIMEOUT,
-                                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-                                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-                        // add basic auth
+                        JsonRequest<JSONArray> req = new ProxyLogRequest();
+                        req.setRetryPolicy(new DefaultRetryPolicy(HTTP_CONNECTION_TIMEOUT,
+                                DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
                         httpRequestQueue.add(req);
                     } catch (Exception e) {
                         Log.e(TAG, e.getMessage(), e);
@@ -208,18 +153,101 @@ public class ProxyNbiService extends AbstractNbiService {
         }
     }
 
-    private class JsonLogRequest extends JsonRequest<JSONArray> {
+    private Map<String, String> addAuthHeaders(Map<String, String> headers) {
+        String creds = String.format("%s:%s", proxy.getUsername(), proxy.getPassword());
+        String auth = "Basic " + Base64.encodeToString(creds.getBytes(), Base64.DEFAULT);
+        headers.put("Authorization", auth);
+        return headers;
+    }
 
-        public JsonLogRequest(int method, String url, String requestBody,
-                Listener<JSONArray> listener, ErrorListener errorListener) {
+    private class ProxySendRequest extends JsonObjectRequest {
+
+        public ProxySendRequest(Request request) {
+            super(POST, proxy.asUrl(), request.toJson(), new Response.Listener<JSONObject>() {
+
+                @Override
+                public void onResponse(JSONObject response) {
+                    // do nothing
+                }
+            }, new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError e) {
+                    Log.e(TAG, e.getMessage(), e);
+                    state = ERROR;
+                    synchronized (serviceExecutor) {
+                        serviceExecutor.notifyAll();
+                    }
+                }
+            });
+        }
+
+        public ProxySendRequest(int method, String url, JSONObject jsonRequest, Listener<JSONObject> listener,
+                ErrorListener errorListener) {
+            super(method, url, jsonRequest, listener, errorListener);
+        }
+
+        @Override
+        public Map<String, String> getHeaders() throws AuthFailureError {
+            Map<String, String> headers = new HashMap<String, String>(super.getHeaders());
+            return addAuthHeaders(headers);
+        }
+    }
+
+    private class ProxyLogRequest extends JsonRequest<JSONArray> {
+
+        public ProxyLogRequest() {
+            super(POST, proxy.asUrl(), new LogRequest(lastMessageTimestamp).toJson().toString(),
+                    new Response.Listener<JSONArray>() {
+
+                        @Override
+                        public void onResponse(JSONArray response) {
+                            try {
+                                for (int i = 0; i < response.length(); i++) {
+                                    JSONObject log = response.getJSONObject(i);
+                                    JSONObject message = log.getJSONObject(MESSAGE_KEY);
+                                    Date timestamp = df.parse(log.getString(TIMESTAMP_KEY));
+                                    processMessage(message, timestamp);
+                                }
+                            } catch (Exception e) {
+                                Log.e(TAG, "failed to process [" + response + "]: " + e.getMessage(), e);
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+
+                        @Override
+                        public void onErrorResponse(VolleyError e) {
+                            Log.e(TAG, e.getMessage(), e);
+                            state = ERROR;
+                            synchronized (serviceExecutor) {
+                                serviceExecutor.notifyAll();
+                            }
+                        }
+                    });
+        }
+
+        public ProxyLogRequest(int method, String url, String requestBody, Listener<JSONArray> listener,
+                ErrorListener errorListener) {
             super(method, url, requestBody, listener, errorListener);
         }
 
         @Override
-        protected Response<JSONArray> parseNetworkResponse(NetworkResponse arg0) {
-            // TODO Auto-generated method stub
-            return null;
+        public Map<String, String> getHeaders() throws AuthFailureError {
+            Map<String, String> headers = new HashMap<String, String>(super.getHeaders());
+            return addAuthHeaders(headers);
         }
 
+        @Override
+        protected Response<JSONArray> parseNetworkResponse(NetworkResponse response) {
+            try {
+                String jsonString = new String(response.data, HttpHeaderParser.parseCharset(response.headers,
+                        PROTOCOL_CHARSET));
+                return Response.success(new JSONArray(jsonString), HttpHeaderParser.parseCacheHeaders(response));
+            } catch (UnsupportedEncodingException e) {
+                return Response.error(new ParseError(e));
+            } catch (JSONException je) {
+                return Response.error(new ParseError(je));
+            }
+        }
     }
 }
