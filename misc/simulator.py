360d03c3cd6aba6bfddc3d6ada20e20e0a5bfcd2
Viacheslav Gagara <viacheslavg@gmail.com>
2017-02-19 11:14:18 +0200

    updated simulator. added python virtualenv for simulator

--- misc/simulator.py
+++ misc/simulator.py
@@ -1,13 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/<cmd>', methods=['GET'])
-@app.route('/<cmd>/<nid>', methods=['GET'])
-@app.route('/<cmd>/<nid>/<nstate>', methods=['GET'])
-@app.route('/<cmd>/<nid>/<nstate>/<nts>', methods=['GET'])
-def get_request(cmd, nid=None, nstate=None, nts=None):
-    return 'cmd: %s; id: %s; state: %s; ts: %s' % (cmd, nid, nstate, nts)
+@app.route("/", methods=['POST'])
+def post_request():
+    print('REQUEST: %s: %s' % (request.headers, request.data))
+    return ('', 200)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
