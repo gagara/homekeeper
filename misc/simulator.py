daccf1c5330417130801d10d9c5f1f4681d5acf8
Viacheslav Gagara <viacheslavg@gmail.com>
2018-04-29 23:37:00 +0300

    add timeout to simulator

--- misc/simulator.py
+++ misc/simulator.py
@@ -1,10 +1,12 @@
 from flask import Flask, request
+import time
 
 app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
 def post_request():
     print('REQUEST: %s: %s' % (request.headers, request.data))
+    time.sleep(3)
     return ('', 200)
 
 if __name__ == "__main__":
