a71661b73b6622a890c4240a65504b8b4e84fdb2
Viacheslav Gagara <viacheslavg@gmail.com>
2019-02-04 17:27:14 +0200

    fixed issue with octal interpretation of minutes with leading zero

--- logserver/scheduler/fronius_pull_stats.sh
+++ logserver/scheduler/fronius_pull_stats.sh
@@ -24,11 +24,11 @@ query_stats() {
 sleep 10
 
 # current stats
-if [ $(($(date +%M) % ${FRONIUS_CURR_RQ_PULL_INTERVAL:-1})) -eq 0 ]; then
+if [ $((10#$(date +%M) % ${FRONIUS_CURR_RQ_PULL_INTERVAL:-1})) -eq 0 ]; then
     query_stats "http://${FRONIUS_HOST:-localhost}:${FRONIUS_PORT:-80}/solar_api/v1/GetInverterRealtimeData.cgi?Scope=Device&DeviceId=1&DataCollection=CommonInverterData" 
 fi
 
 # archive stats
-if [ $(($(date +%M) % ${FRONIUS_ARCH_RQ_PULL_INTERVAL:-1})) -eq 0 ]; then
+if [ $((10#$(date +%M) % ${FRONIUS_ARCH_RQ_PULL_INTERVAL:-1})) -eq 0 ]; then
     query_stats "http://${FRONIUS_HOST:-localhost}:${FRONIUS_PORT:-80}/solar_api/v1/GetArchiveData.cgi?Scope=Device&DeviceClass=Inverter&DeviceId=1&StartDate=$(date -u --date=@$(($(date -u +%s) - $((${FRONIUS_ARCH_RQ_PULL_INTERVAL:-1} * 60)))) +%Y-%m-%dT%H:%M:%S)&EndDate=$(date -u +%Y-%m-%dT%H:%M:%S)&Channel=Current_DC_String_1&Channel=Current_DC_String_2&Channel=Voltage_DC_String_1&Channel=Voltage_DC_String_2&Channel=Temperature_Powerstage" 
 fi
